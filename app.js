4d585f8b0a97214951f76dad02edbc72f7eede83
Y********* <alinr10@hotmail.com>
2023-10-13 16:29:16 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app=express()
+
+const port=3000
+
+app.get('/',(req,res)=>{
+
+    res.send('Ana sayfa')
+
+})
+
+
+app.listen(port,()=>{
+
+    console.log(`Application running on port: ${port}`)
+
+})
